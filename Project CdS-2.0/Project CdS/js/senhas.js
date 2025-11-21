fd6692ed1bcba6e928863742995ad15cd2ee509a
Amayya <105234629+danieleheckel@users.noreply.github.com>
2025-11-21 11:30:18 -0300

    Add files via upload

---
+++ Project CdS-2.0/Project CdS/js/senhas.js
@@ -0,0 +1,16 @@
+function verificarSenha() {
+    var senhasCorretas = {
+        "lirio de sangue": "segredo/BlutLilien.html",
+        "ela foi a primeira": "segredo/",
+    };
+
+    var senhaDigitada = document.getElementById ("senha").value.trim();
+
+    if (senhasCorretas.hasOwnProperty(senhaDigitada)) {
+        console.log("Redirecionando para:", senhasCorretas[senhaDigitada]);
+        window.location.href = senhasCorretas[senhaDigitada];
+    } else {
+        console.error("Senha incorreta:", senhaDigitada);
+        alert("Tente outro");
+    }
+}
