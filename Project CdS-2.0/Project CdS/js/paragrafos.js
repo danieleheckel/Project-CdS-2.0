8a5ae718df69ced3fd00962e1eda512ac38636ac
Amayya <105234629+danieleheckel@users.noreply.github.com>
2025-11-21 11:33:24 -0300

    Delete Project CdS-2.0 directory

--- Project CdS-2.0/Project CdS/js/paragrafos.js
+++
@@ -1,8 +0,0 @@
-function paragrafo() {
-  const p = document.getElementById("paragrafo");
-  if (p.style.display === "none") {
-    p.style.display = "block";
-  } else {
-    p.style.display = "none";
-  }
-}
