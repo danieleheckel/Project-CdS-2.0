6cbc67f8ba28bb2271e281d7bbb0dc279a81e42f
Amayya <105234629+danieleheckel@users.noreply.github.com>
2025-11-21 11:54:32 -0300

    Add files via upload

---
+++ js/paragrafos.js
@@ -0,0 +1,8 @@
+function paragrafo() {
+  const p = document.getElementById("paragrafo");
+  if (p.style.display === "none") {
+    p.style.display = "block";
+  } else {
+    p.style.display = "none";
+  }
+}
